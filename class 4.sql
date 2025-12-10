a2604847482f046daad004daf39b22be07b88490
Bismi <bismiasbismi@gmail.com>
2025-12-10 12:57:35 +0530

    class 4

---
+++ class 4.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO mobiles VALUES(1, 'Samsung', 'GalaxyM14', 12000, 30),
+ (2, 'Redmi', 'Note 12', 15000, 25),
+  (3, 'Realme', 'Narzo 50', 13000, 20), 
+  (4, 'Samsung', 'Galaxy A23', 18000, 10);
+SELECT * FROM mobiles WHERE price > 13000 OR stock < 15;
+UPDATE mobiles SET stock = stock + 5, price = 12500 WHERE model = 'Narzo 50';
+DELETE FROM mobiles WHERE id = 2;
+SELECT MIN(price) FROM mobiles;
+SELECT MAX(price) FROM mobiles;
+SELECT SUM(stock) FROM mobiles;
+Expand
+SELECT * FROM mobiles LIMIT 2;
